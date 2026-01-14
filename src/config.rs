ed4742109ace8f8446a54546687c9e6e4c184630
rebenkoy <yarebenko@gmail.com>
2026-01-14 23:39:54 +0400

    Allow override configs from command line flags

--- src/config.rs
+++ src/config.rs
@@ -1,117 +1,238 @@
-use std::path::Prefix;
+use conf::{Conf, ConfContext, ConfSerde, ConfSerdeContext, Error, InitializationStateMachine, InnerError, NextValueProducer, ParsedEnv, Parser, ParserConfig, ProgramOption};
+use conf::lazybuf::LazyBuf;
 use serde::{Deserialize, Serialize};
-use clap::{ArgMatches, Args, Command, Error, FromArgMatches};
-use clap::builder::Resettable;
 use minijinja::value::Object;
+use partially::Partial;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+pub trait Mergable {
+    type Partial;
+    fn merge(&mut self, part: Self::Partial);
+}
+
+#[derive(Partial)]
+#[partially(derive(Conf, Debug))]
+#[derive(Debug, Clone, Serialize, Deserialize, Conf)]
 pub struct reSsgConfig {
+    #[partially(as_type = "Option<PartialServerConfig>")]
+    #[conf(flatten, long_prefix="server.")]
     pub server: ServerConfig,
+    #[partially(as_type = "Option<PartialBuildConfig>")]
+    #[conf(flatten, long_prefix="build.")]
     pub build: BuildConfig,
 }
-#[derive(Debug, Clone, Serialize, Deserialize)]
+
+impl Mergable for reSsgConfig {
+    type Partial = PartialreSsgConfig;
+    fn merge(&mut self, part: Self::Partial) {
+        part.server.map(|c| self.server.merge(c));
+        part.build.map(|c| self.build.merge(c));
+    }
+}
+
+#[derive(Partial)]
+#[partially(derive(Conf, Debug))]
+#[derive(Debug, Clone, Serialize, Deserialize, Conf)]
 pub struct ServerConfig {
+    #[partially(as_type = "Option<PartialControlConfig>")]
+    #[conf(flatten, long_prefix="control.")]
     pub control: ControlConfig,
+    #[partially(as_type = "Option<PartialEndpointConfig>")]
+    #[conf(flatten, long_prefix="output.")]
     pub output: EndpointConfig,
+    #[conf(repeat, long)]
+    #[partially(omit)]
     pub watch_excludes: Vec<String>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+impl Mergable for ServerConfig {
+    type Partial = PartialServerConfig;
+    fn merge(&mut self, part: Self::Partial) {
+        part.control.map(|c| self.control.merge(c));
+        part.output.map(|c| self.output.merge(c));
+    }
+}
+
+impl From<PartialServerConfig> for ServerConfig {
+    fn from(s: PartialServerConfig) -> Self {
+        todo!()
+    }
+}
+
+#[derive(Partial)]
+#[partially(derive(Conf, Debug))]
+#[derive(Debug, Clone, Serialize, Deserialize, Conf)]
 pub struct EndpointConfig {
+    #[arg(long)]
     pub port: String,
+    #[arg(long)]
     pub interface: String,
+}
+
+impl Mergable for EndpointConfig {
+    type Partial = PartialEndpointConfig;
+    fn merge(&mut self, part: Self::Partial) {
+        part.port.map(|p| self.port = p);
+        part.interface.map(|i| self.interface = i);
+    }
+}
+
+impl From<PartialEndpointConfig> for EndpointConfig {
+    fn from(p: PartialEndpointConfig) -> Self {
+        p.port.map(|port|
+            p.interface.map(|interface| {
+                Self {
+                    port,
+                    interface,
+                }
+            }).expect("Unsaturated partial conversion")
+        ).expect("Unsaturated partial conversion")
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum ControlConfig {
     None,
     Endpoint(EndpointConfig),
-    Prefix(String)
+    Prefix(String),
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+impl Mergable for ControlConfig {
+    type Partial = PartialControlConfig;
+    fn merge(&mut self, part: Self::Partial) {
+        match (part.endpoint, part.prefix) {
+            (None, None) => {
+                return;
+            }
+            (None, Some(prefix)) => {
+                *self = Self::Prefix(prefix);
+            }
+            (Some(part), None) => {
+                match self {
+                    ControlConfig::None => {}
+                    ControlConfig::Endpoint(ep) => {
+                        ep.merge(part);
+                    }
+                    ControlConfig::Prefix(_) => {
+                        Self::Endpoint(part.into());
+                    }
+                }
+            }
+            (Some(p), Some(p_prefix)) => {
+                panic!("Double assignment to control config")
+            }
+        }
+    }
+}
+
+#[derive(Debug, Conf)]
+#[conf(at_most_one_of_fields(endpoint, prefix))]
+pub struct PartialControlConfig {
+    #[conf(flatten, long_prefix = "endpoint.")]
+    endpoint: Option<PartialEndpointConfig>,
+    #[arg(long)]
+    prefix: Option<String>,
+}
+
+impl From<PartialControlConfig> for ControlConfig {
+    fn from(value: PartialControlConfig) -> Self {
+        if let Some(config) = value.endpoint {
+            ControlConfig::Endpoint(config.try_into().expect("Not full endpoint config"))
+        } else if let Some(config) = value.prefix {
+            ControlConfig::Prefix(config)
+        } else {
+            ControlConfig::None
+        }
+    }
+}
+
+impl Conf for ControlConfig {
+    fn debug_asserts() {
+        PartialControlConfig::debug_asserts();
+    }
+
+    fn get_parser_config() -> Result<ParserConfig, Error> {
+        PartialControlConfig::get_parser_config()
+    }
+
+    const PROGRAM_OPTIONS: LazyBuf<ProgramOption> = PartialControlConfig::PROGRAM_OPTIONS;
+
+    fn get_subcommands(parsed_env: &ParsedEnv) -> Result<Vec<Parser>, Error> {
+        PartialControlConfig::get_subcommands(parsed_env)
+    }
+
+    fn from_conf_context(conf_context: ConfContext<'_>) -> Result<Self, Vec<InnerError>> {
+        let x = PartialControlConfig::from_conf_context(conf_context).map(|x| x.into());
+        println!("{:#?}", x);
+        x
+    }
+
+    fn get_name() -> &'static str {
+        PartialControlConfig::get_name()
+    }
+}
+
+#[derive(Partial)]
+#[partially(derive(Conf, Debug))]
+#[derive(Debug, Clone, Serialize, Deserialize, Conf)]
 pub struct BuildConfig {
+    #[arg(long)]
     pub source: String,
+    #[arg(long)]
     pub index_toml_name: String,
+    #[arg(long)]
     pub output: String,
+    #[arg(long)]
     pub prefix: String,
+    #[arg(long)]
     pub static_path: String,
+    #[arg(long)]
     pub static_output: String,
+    #[partially(as_type = "Option<PartialSassConfig>")]
+    #[conf(flatten, long_prefix="sass.")]
     pub sass: SassConfig,
+}
+
+impl Mergable for BuildConfig {
+    type Partial = PartialBuildConfig;
+
+    fn merge(&mut self, part: Self::Partial) {
+        part.source.map(|p| self.source = p);
+        part.index_toml_name.map(|p| self.index_toml_name = p);
+        part.output.map(|p| self.output = p);
+        part.prefix.map(|p| self.prefix = p);
+        part.static_path.map(|p| self.static_path = p);
+        part.static_output.map(|p| self.static_output = p);
+        part.sass.map(|p| self.sass.merge(p));
+    }
+}
+
+impl From<PartialBuildConfig> for BuildConfig {
+    fn from(value: PartialBuildConfig) -> Self {
+        panic!()
+    }
 }
 
 impl Object for BuildConfig {}
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Partial)]
+#[partially(derive(Conf, Debug))]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Conf)]
 pub struct SassConfig {
+    #[arg(long)]
     pub source: String,
+    #[arg(long)]
     pub destination: String,
 }
 
-
-//
-// impl FromArgMatches for ServerConfig {
-//     fn from_arg_matches(matches: &ArgMatches) -> Result<Self, Error> {
-//         todo!()
-//     }
-//
-//     fn update_from_arg_matches(&mut self, matches: &ArgMatches) -> Result<(), Error> {
-//         todo!()
-//     }
-// }
-//
-// impl Args for ServerConfig {
-//     fn augment_args(mut cmd: Command) -> Command {
-//         fn add_prefix(prefix: &'static str, value: String) -> String {
-//             format!("{}-{}", prefix, value)
-//         }
-//
-//         let socket = EndpointConfig::augment_args(Command::new("socketConfig"));
-//         let socket_args = socket.get_arguments().cloned().collect::<Vec<_>>();
-//         let socket_groupd = socket.get_groups().cloned().collect::<Vec<_>>();
-//
-//         for arg in socket_args {
-//             let long = add_prefix("socket", arg.get_long().unwrap().to_string());
-//             let id = add_prefix("socket", arg.get_id().to_string());
-//             cmd = cmd.arg(arg.id(id).long(long).short(Resettable::Reset));
-//         }
-//         for mut group in socket_groupd {
-//             cmd = cmd.group(
-//                 clap::ArgGroup::new(add_prefix("socket", group.get_id().to_string()))
-//                     .multiple(group.is_multiple())
-//                     .args({
-//                         group
-//                             .get_args()
-//                             .map(|arg| clap::Id::from(add_prefix("socket", arg.to_string())))
-//                     })
-//             )
-//         }
-//
-//         let output = EndpointConfig::augment_args(Command::new("outputConfig"));
-//         let output_args = output.get_arguments().cloned().collect::<Vec<_>>();
-//         let output_groupd = output.get_groups().cloned().collect::<Vec<_>>();
-//
-//         for arg in output_args {
-//             let long = add_prefix("output", arg.get_long().unwrap().to_string());
-//             let id = add_prefix("output", arg.get_id().to_string());
-//             cmd = cmd.arg(arg.id(id).long(long).short(Resettable::Reset));
-//         }
-//         for mut group in output_groupd {
-//             cmd = cmd.group(
-//                 clap::ArgGroup::new(add_prefix("output", group.get_id().to_string()))
-//                     .multiple(group.is_multiple())
-//                     .args({
-//                         group
-//                             .get_args()
-//                             .map(|arg| clap::Id::from(add_prefix("output", arg.to_string())))
-//                     })
-//             )
-//         }
-//
-//         cmd
-//     }
-//
-//     fn augment_args_for_update(cmd: Command) -> Command {
-//         todo!()
-//     }
-// }
+impl Mergable for SassConfig {
+    type Partial = PartialSassConfig;
+    fn merge(&mut self, part: Self::Partial) {
+        part.source.map(|p| self.source = p);
+        part.destination.map(|p| self.destination = p);
+    }
+}
+impl From<PartialSassConfig> for SassConfig {
+    fn from(value: PartialSassConfig) -> Self {
+        todo!()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,90 @@ mod cli_parser;
 mod server;
 mod util;
 
-use clap::{Parser, Subcommand};
+use clap::arg;
+use conf::{Conf, Subcommands};
+use partially::Partial;
+use toml::de::Error;
+use toml::Deserializer;
 use crate::build::build;
+use crate::Command::Build;
 use crate::server::serve;
-use crate::config::reSsgConfig;
+use crate::config::{reSsgConfig, PartialreSsgConfig, PartialBuildConfig, Mergable};
+
 
 /// Simple program to greet a person
-#[derive(Parser, Debug)]
-struct Args {
-    #[clap(subcommand)]
+#[derive(Debug)]
+pub struct Arguments {
     command: Command
 }
 
-#[derive(Subcommand, Debug)]
+impl ::conf::Conf for Arguments {
+    fn get_parser_config() -> Result<::conf::ParserConfig, ::conf::Error> {
+        let parser_config = conf::ParserConfig { about: Some("Simple program to greet a person"), name: "reSsg", no_help_flag: false, styles: None, version: None };
+        Ok(parser_config)
+    }
+    const PROGRAM_OPTIONS: ::conf::lazybuf::LazyBuf<::conf::ProgramOption> = {
+        <PartialreSsgConfig as Conf>::PROGRAM_OPTIONS
+    };
+    fn get_subcommands(__parsed_env__: &::conf::ParsedEnv) -> Result<Vec<::conf::Parser>, ::conf::Error> {
+        let mut __parsers__ = vec![];
+        if !__parsers__.is_empty() { panic!("Not supported to have multiple subcommands fields on the same struct: at field 'command'"); }
+        __parsers__.extend(<Command
+        as ::conf::Subcommands>::get_parsers(__parsed_env__)?);
+        Ok(__parsers__)
+    }
+    fn from_conf_context<'a>(__conf_context__: ::conf::ConfContext<'a>) -> Result<Self, Vec<::conf::InnerError>> {
+        let mut __errors__ = Vec::<::conf::InnerError>::new();
+        let __conf_context__ = &__conf_context__;
+        let command = {
+            fn command(__conf_context__: &::conf::ConfContext<'_>) -> Result<Command
+                , ::std::vec::Vec<::conf::InnerError>> {
+                use ::conf::{InnerError, Subcommands};
+                let Some((name, conf_context)) = __conf_context__.for_subcommand() else {
+                    return Err(vec![InnerError::missing_required_subcommand("Arguments", "command", <Command
+                    as Subcommands>::get_subcommand_names())]);
+                };
+                <Command
+                as Subcommands>::from_conf_context(name, conf_context)
+            }
+            match command(__conf_context__) {
+                Ok(val) => Some(val),
+                Err(errs) => {
+                    __errors__.extend(errs);
+                    None
+                }
+            }
+        };
+        if !__errors__.is_empty() { return Err(__errors__); }
+        let return_value = match (command) {
+            ( Some(command) ) => Arguments {
+                command
+            },
+            _ => panic!("Internal error: no errors encountered but struct was incomplete")
+        };   fn validation<'ctxctx>(__instance__: &Arguments, __conf_context__: &::conf::ConfContext<'ctxctx>) -> Result<(), Vec<::conf::InnerError>> { Ok(()) }
+        validation(&return_value, __conf_context__)?;
+        Ok(return_value)
+    }
+    fn get_name() -> &'static str { "Arguments" }
+    fn debug_asserts() {
+        {
+            let mut short_forms = ::std::collections::HashMap::<char, String>::new();
+            for opt in Self::PROGRAM_OPTIONS.iter() { if let Some(short) = opt.short_form { if let Some(existing_id) = short_forms.insert(short, opt.id.to_string()) { panic!("Short option '{}' is used by both '{}' and '{}' in {}", short, existing_id, opt.id, stringify!(Self )); } } }
+        }
+        <Command
+        as ::conf::Subcommands>::debug_asserts();
+    }
+}
+
+#[derive(Subcommands, Debug)]
 enum Command {
-    Build,
-    Serve,
+    Build(PartialBuildConfig),
+    Serve(PartialreSsgConfig),
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let args = Args::parse();
+    let args = Arguments::parse();
+
     std::env::var("RESSG_ROOT").map_or(Ok(()), |dir| {
         std::env::set_current_dir(dir)
     })?;
@@ -31,12 +95,14 @@ fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let config_file = std::env::current_dir()?
         .join("config.toml");
 
-    let config: reSsgConfig = toml::from_slice(&std::fs::read(config_file)?)?;
+    let mut config: reSsgConfig = toml::from_slice(&std::fs::read(config_file)?)?;
     match args.command {
-        Command::Build => {
-            build(&config.build, &mut rsfs::disk::FS{})?;
+        Command::Build(cfg) => {
+            config.build.merge(cfg);
+            build(&config.build, &mut rsfs::disk::FS {})?;
         }
-        Command::Serve => {
+        Command::Serve(cfg) => {
+            config.merge(cfg);
             serve(&config)?;
         }
     }
